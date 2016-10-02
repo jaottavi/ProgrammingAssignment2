6e8183cf31267b2da6adecd60d7245bbe6b71960
jaottavi <ottavijames@gmail.com>
2016-10-02 12:23:35 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions are designed to both solve the inverse of inputted matrices (if invertible) and
+## to cache/store the information for that invertible matrix once the computation is done. This is being done
+## so as to make it easier to retrieve solutions if the solution has already been computed previously.
 
-## Write a short comment describing this function
+## makeCacheMatrix take a given matrix and sets up a list that stores the information for that given matrix
+## and it's inverse for later use
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL # create object m within the makeCacheMatrix environment
+  set <- function(y) { # function sets/stores matrix wi
+    x <<- y # set 
+    m <<- NULL # set m to NULL in PARENT environment (makeCacheMtrix environment
+  }
+  get <- function() x # gets x
+  setsolve <- function(inverse) m <<- inverse # sets the inverse
+  getsolve <- function() m # gets the inverse 
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve) # list created that set matrix and solution for easier future retrieval
 }
 
 
-## Write a short comment describing this function
+## Returns a matrix that is the inverse of 'x'; if x already previously computed
+## then solution is retrieved from list of makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve() # retrieve getsolve from list
+  if(!is.null(m)) { # check to see if x is in list
+    message("getting cached data") # if TRUE, then solution retrieved
+    return(m) # return solution and bypass repeated calculation
+  }
+  data <- x$get() # if not, then data is gathered and solved
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
